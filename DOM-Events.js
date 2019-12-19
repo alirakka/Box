3c9ef59e3bb8ce847326b2e6cee11cf29b12caf1
alirakka <ali_rakka2001@hotmail.com>
2019-12-19 13:50:01 +0530

    V1

---
+++ DOM-Events.js
@@ -0,0 +1,8 @@
+function randomize() {
+  document.getElementById("first").style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+function randomize2() {
+  document.getElementById("second").style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
